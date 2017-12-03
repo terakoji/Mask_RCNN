5ff14f7a1f828c9eb2b8a11b27ec48f4139b5395
寺田　耕志 <koji_terada@mail.toyota.co.jp>
2017-12-03 20:54:21 +0900

    add package files

---
+++ setup.py
@@ -0,0 +1,21 @@
+setup(
+    name='mask_rcnn',
+    version='2.0.0',
+    description='Mask R-CNN for Object Detection and Segmentation',
+    long_description=readme,
+    author='Waleed Abdulla',
+    author_email='waleed.abdulla@gmail.com',
+    install_requires=['tesnorflow',
+                      'scikit-image',
+                      'docopt',
+                      'clint',
+                      'tablib',
+                      'cython',
+                      'keras',
+                      'tensorflow']
+    dependency_links=['git+https://github.com/pdollar/coco.git#egg=pycocotools&subdirectory=PythonAPI']
+
+    url='https://github.com/matterport/Mask_RCNN',
+    license=MIT,
+    packages=find_packages(exclude=('images', 'assets'))
+)
